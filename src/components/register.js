fcbe97719d48d5b1de73dd09d3f21f55c26c9eac
Frank Fernandez <frankfernogando@gmail.com>
2021-04-11 15:48:43 -0400

    add register

---
+++ src/components/register.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
+
+class Register extends Form {
+  state = { data: { username: "", password: "", name: "" }, errors: {} };
+  schema = {
+    username: Joi.string().required().label("Username").email(),
+    password: Joi.string().required().label("Password").min(5),
+    name: Joi.string().required().label("Name"),
+  };
+  doSubmit = () => {};
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
